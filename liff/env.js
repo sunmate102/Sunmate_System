58f62828e4a3d1712e3e19876c6e286091951256
taiga-automate-growth <oshima@automate-growth.com>
2024-11-30 20:27:54 +0900

    Update env.js

--- liff/env.js
+++ liff/env.js
@@ -8,5 +8,5 @@ const entryId = "2006623345-803p9Kyv";
 const requestSupportId = "2006623345-AMpl8b0z";
 const matchingId = "2006623345-JZmaA8yx";
 const clientMyPageId = "2006623345-kBW4pLzD";
-const gasDeployId = "10H-wiZEmRR4bfiFXadluAxt2AzRqpA9yxw-CYheRYTLTdEh_DWEpUIWa";
+const gasDeployId = "1fx90l3w38kRyRV2iQIltZUAZ4-n2uq12FPtlnLI84YL2Vag_uqa9Pwww";
 export {registerClientId, registerGuideId, editGuideId, clientMessageId, guideMessageId, createReportId, entryId, requestSupportId, matchingId, clientMyPageId , gasDeployId}
